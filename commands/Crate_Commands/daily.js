a74018a1ee8f3cc12fe6c5becf3930d23fa922cc
Sapphirix Client <60874772+ChaotiqueSlime@users.noreply.github.com>
2021-01-06 10:06:55 -0800

    fixed crates

--- commands/Crate_Commands/daily.js
+++ commands/Crate_Commands/daily.js
@@ -10,7 +10,7 @@ module.exports = {
         const first = await DB.GetCash(message.author.id);
         console.log(`${message.author.username} First Timer Check: ${first.cash}`)
         const cratesicon = (`https://images-ext-2.discordapp.net/external/fDqPhcYfKo-97vHDCdJW_mJSTUmxYcfmImIGsRVdA5k/%3Ftoken%3DeyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvZDQ0ZThlNGMtZGIzNC00MzYxLTg1MDgtZTcyODhkNGNjMTBmXC9kNTFidnBlLWE2YzdjZTI3LTA5ODgtNDM3NS1hOTAyLWFiZTY3YTgwYjYwNS5naWYifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.OHaADKw8oSYtPeXCHb51Fk2pTCOyjoRRs2aDbgiGo0g/https/images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d44e8e4c-db34-4361-8508-e7288d4cc10f/d51bvpe-a6c7ce27-0988-4375-a902-abe67a80b605.gif`)
-        DB.AddCash(message.author.id, 1000000)
+        await DB.AddCash(message.author.id, 1000000)
         var output = await DB.GetCash(message.author.id);
         const formatNumber = require('../../functions/regex')
         const embed = new Discord.MessageEmbed()
--- commands/Crate_Commands/member.js
+++ commands/Crate_Commands/member.js
@@ -5,25 +5,44 @@ module.exports = {
     cooldown: 604800,
     async execute(message) {
         if(message.deletable) message.delete()
-        const Discord = require('discord.js')
+        const { MessageEmbed } = require('discord.js')
         const DB = require('djs-economy')
+        const formatNumber = require('../../functions/regex');
         const cratesicon = (`https://images-ext-2.discordapp.net/external/fDqPhcYfKo-97vHDCdJW_mJSTUmxYcfmImIGsRVdA5k/%3Ftoken%3DeyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvZDQ0ZThlNGMtZGIzNC00MzYxLTg1MDgtZTcyODhkNGNjMTBmXC9kNTFidnBlLWE2YzdjZTI3LTA5ODgtNDM3NS1hOTAyLWFiZTY3YTgwYjYwNS5naWYifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.OHaADKw8oSYtPeXCHb51Fk2pTCOyjoRRs2aDbgiGo0g/https/images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d44e8e4c-db34-4361-8508-e7288d4cc10f/d51bvpe-a6c7ce27-0988-4375-a902-abe67a80b605.gif`);
-        const boostersrole = message.guild.roles.cache.find(r => r.name === "Patrons");
-        if(message.member.roles.cache.has(boostersrole.id)) {
-          await DB.AddCash(message.author.id, 10000000)
-          const output = await DB.GetCash(message.author.id);
-            const formatNumber = require('../../functions/regex');
-            const embed = new Discord.MessageEmbed()
-                .setColor('#cf13f0')
-                .setThumbnail(cratesicon)
-                .setDescription(`:dollar: **10,000,000** was added to your balance\nYou Now Have $${(formatNumber(output.cash))}`)
-                .setTitle('Member Crate Claimed');
-                message.channel.send(embed);
-            } else {
-            return message.channel.send(`You are not a youtube member ${message.author}`).then((msg) => {
-                msg.delete({ timeout: 8000 })
-            })
-        }
+        
+        const MembersRole = message.guild.roles.cache.find(r => r.name === "YouTube Member");
+        const MembersPlusRole = message.guild.roles.cache.find(r => r.name === "YouTube Member");
+        const MembersPlusPlusRole = message.guild.roles.cache.find(r => r.name === "YouTube Member");
+
+        const embed = new MessageEmbed()
+        .setColor('#cf13f0')
+        .setThumbnail(cratesicon)
+        .setTitle('Member Crate Claimed');
+
+
+        if(message.member.roles.cache.has(MembersRole.id)) {
+        await DB.AddCash(message.author.id, 10000000)
+        const output = await DB.GetCash(message.author.id);
+        embed.setDescription(`💵 **10,000,000** was added to your balance\nYou Now Have $${(formatNumber(output.cash))}`)
+        return message.channel.send(embed);} 
+
+        if(message.member.roles.cache.has(MembersPlusRole.id)) {
+        await DB.AddCash(message.author.id, 10000000)
+        const output = await DB.GetCash(message.author.id);
+        embed.setDescription(`💵 **10,000,000** was added to your balance\nYou Now Have $${(formatNumber(output.cash))}`)
+        return message.channel.send(embed);} 
+
+        if(message.member.roles.cache.has(MembersPlusPlusRole.id)) {
+        await DB.AddCash(message.author.id, 10000000)
+        const output = await DB.GetCash(message.author.id);
+        embed.setDescription(`💵 **10,000,000** was added to your balance\nYou Now Have $${(formatNumber(output.cash))}`)
+        return message.channel.send(embed);} 
+    
+
+
+
+        return message.channel.send(`You're not a YouTube Member <@${message.author.id}>`).then((msg) => {msg.delete({ timeout: 8000 })})
+        
 
     }
 }
