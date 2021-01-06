e7882590b4dc5bf7368ab8d80232c311e4c59493
Sapphirix Client <60874772+ChaotiqueSlime@users.noreply.github.com>
2021-01-06 10:46:46 -0800

    More Effiecent Improvements

--- commands/Crate_Commands/member.js
+++ commands/Crate_Commands/member.js
@@ -11,35 +11,25 @@ module.exports = {
         const cratesicon = (`https://images-ext-2.discordapp.net/external/fDqPhcYfKo-97vHDCdJW_mJSTUmxYcfmImIGsRVdA5k/%3Ftoken%3DeyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvZDQ0ZThlNGMtZGIzNC00MzYxLTg1MDgtZTcyODhkNGNjMTBmXC9kNTFidnBlLWE2YzdjZTI3LTA5ODgtNDM3NS1hOTAyLWFiZTY3YTgwYjYwNS5naWYifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.OHaADKw8oSYtPeXCHb51Fk2pTCOyjoRRs2aDbgiGo0g/https/images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d44e8e4c-db34-4361-8508-e7288d4cc10f/d51bvpe-a6c7ce27-0988-4375-a902-abe67a80b605.gif`);
         
         const MembersRole = message.guild.roles.cache.find(r => r.name === "YouTube Member");
-        const MembersPlusRole = message.guild.roles.cache.find(r => r.name === "YouTube Member");
-        const MembersPlusPlusRole = message.guild.roles.cache.find(r => r.name === "YouTube Member");
+        const MembersPlusRole = message.guild.roles.cache.find(r => r.name === "YouTube Member+");
+        const MembersPlusPlusRole = message.guild.roles.cache.find(r => r.name === "YouTube Member++");
 
         const embed = new MessageEmbed()
         .setColor('#cf13f0')
         .setThumbnail(cratesicon)
         .setTitle('Member Crate Claimed');
 
+        let hasPermission = false;
 
-        if(message.member.roles.cache.has(MembersRole.id)) {
-        await DB.AddCash(message.author.id, 10000000)
-        const output = await DB.GetCash(message.author.id);
-        embed.setDescription(`💵 **10,000,000** was added to your balance\nYou Now Have $${(formatNumber(output.cash))}`)
-        return message.channel.send(embed);} 
+        if(MembersRole){if(message.member.roles.cache.has(MembersRole.id)) hasPermission = true;}
+        if(MembersPlusRole){if(message.member.roles.cache.has(MembersPlusRole.id)) hasPermission = true;}
+        if(MembersPlusPlusRole){if(message.member.roles.cache.has(MembersPlusPlusRole.id)) hasPermission = true;}
 
-        if(message.member.roles.cache.has(MembersPlusRole.id)) {
-        await DB.AddCash(message.author.id, 10000000)
-        const output = await DB.GetCash(message.author.id);
-        embed.setDescription(`💵 **10,000,000** was added to your balance\nYou Now Have $${(formatNumber(output.cash))}`)
-        return message.channel.send(embed);} 
-
-        if(message.member.roles.cache.has(MembersPlusPlusRole.id)) {
-        await DB.AddCash(message.author.id, 10000000)
-        const output = await DB.GetCash(message.author.id);
-        embed.setDescription(`💵 **10,000,000** was added to your balance\nYou Now Have $${(formatNumber(output.cash))}`)
-        return message.channel.send(embed);} 
-    
-
-
+        if(hasPermission) {
+            await DB.AddCash(message.author.id, 10000000)
+            const output = await DB.GetCash(message.author.id);
+            embed.setDescription(`💵 **10,000,000** was added to your balance\nYou Now Have $${(formatNumber(output.cash))}`)
+        return message.channel.send(embed);}
 
         return message.channel.send(`You're not a YouTube Member <@${message.author.id}>`).then((msg) => {msg.delete({ timeout: 8000 })})
         
--- commands/Other_Commands/nickname.js
+++ commands/Other_Commands/nickname.js
@@ -5,16 +5,30 @@ module.exports = {
     cooldown: 5,
     async execute(message, args) {
         if(message.deletable) message.delete()
-        const role = message.guild.roles.cache.find(r => r.name === "Moderator");
-        if(!message.member.roles.cache.has(role.id)) return;
+
+        const ModRole = message.guild.roles.cache.find(r => r.name === "Moderator");
+        const MembersRole = message.guild.roles.cache.find(r => r.name === "YouTube Member");
+        const MembersPlusRole = message.guild.roles.cache.find(r => r.name === "YouTube Member+");
+        const MembersPlusPlusRole = message.guild.roles.cache.find(r => r.name === "YouTube Member++");
+
+        let hasPermission = false;
+
+        if(ModRole){if(message.member.roles.cache.has(ModRole.id)) hasPermission = true;}
+        if(MembersRole){if(message.member.roles.cache.has(MembersRole.id)) hasPermission = true;}
+        if(MembersPlusRole){if(message.member.roles.cache.has(MembersPlusRole.id)) hasPermission = true;}
+        if(MembersPlusPlusRole){if(message.member.roles.cache.has(MembersPlusPlusRole.id)) hasPermission = true;}
+
+        if (hasPermission) {
         const nickname = args.join(" ");
 
         if(!args[0]){return message.channel.send(`Your nickname cant be blank`).then(m => m.delete({ timeout: 8000 }));}
         if(args[5]){return message.channel.send(`Theres to many spaces in your nickname`).then(m => m.delete({ timeout: 8000 }));}
 
-        const reason = `Member Perk`
+        const reason = `Nickname Member Perk`
         message.member.setNickname(`${nickname}`, reason);
 
-        return message.channel.send(`<@${message.author.id}> Your Nickname Has Been Updated!`);
+        return message.channel.send(`<@${message.author.id}> Your Nickname Has Been Updated!, To - ${nickname}`);
+        }
+        return;
     }
 }
